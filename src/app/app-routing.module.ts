5027ef0431fb4173ec6f9f3e577eccb035d7cb03
Gulsen <gulhelince@hotmail.com>
2024-11-05 12:02:42 +0300

    Routing yapılanması.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ProductOverviewComponent } from './components/product-overview/product-overview.component';
+import { ProductSpecComponent } from './components/product-spec/product-spec.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //default route ayarlamak
+  {path:"",redirectTo:"/home",pathMatch:"full"},
+  {path:"home",component:HomeComponent},
+  {path:"home/:id",component:HomeComponent}, //router parametresi tanımlandı
+  {path:"about",component:AboutComponent},
+  {path:"contact",component:ContactComponent},
+
+  //Child Routes
+  //child route altında child route tanımlamma
+  {path:"products",component:ProductComponent,
+    children:[
+      {path:"detail/:id",component:ProductDetailComponent,
+        children:[
+          {path:"",redirectTo:"overview",pathMatch:"full"},//default olarak çağrılır
+          {path:"overview",component:ProductOverviewComponent},
+          {path:"spec",component:ProductSpecComponent}
+        ]
+      }
+    ]
+  },
+
+
+  //hiç bir path'e uygun olmayan istek geldiğinde 
+  {path:"**", component:ErrorComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { RouterModule } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ProductSpecComponent } from './components/product-spec/product-spec.component';
+import { ProductOverviewComponent } from './components/product-overview/product-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ErrorComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    ProductSpecComponent,
+    ProductOverviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
